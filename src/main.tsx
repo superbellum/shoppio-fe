27f35b9d85acf98df1ef2d14eb5f5a593669328f
Patrik Gugh <patrik.gugh@materna.group>
2025-11-12 03:30:59 +0100

    Set up prime react & redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import type { ReactNode } from "react";
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 
-createRoot(document.getElementById('root')!).render(
+const rootNode = (
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <PrimeReactProvider>
+      <App/>
+    </PrimeReactProvider>
+  </StrictMode>
+) as ReactNode;
+
+createRoot(document.getElementById("root")!).render(rootNode);
